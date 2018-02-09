38850a73ab70fc628996c5f56c7d51d523e54393
papahippo <hippostech@gmail.com>
2018-02-09 22:51:52 +0100

    improved sample euphonia mail. incubating a copy of this in  admin test directory!

---
+++ phileas/admin/test/proms_mail_42.py
@@ -0,0 +1,22 @@
+from Euphonia import *
+
+
+class ThisMailing(Mailing):
+    def get_html_text(self, recipients=[], mailGroup=None, file_list=[], taal='NL'):
+        names = [recipient.roepnaam for recipient in recipients]
+        name_str = ', '.join(names[:-1])
+        if name_str:
+            name_str += names[-1]
+        else:
+            name_str = names[-1]
+
+        return h |(
+            "Dear ",  name_str,
+            h.br*2,
+            "something seems to be actually ",
+            h.b | "working!"
+        )
+
+
+if __name__ == "__main__":
+    ThisMailing().main()
