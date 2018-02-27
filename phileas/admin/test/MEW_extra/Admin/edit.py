a99b085ee2c686b4db405ced216b40a4fcc14448
papahippo <hippostech@gmail.com>
2018-02-27 16:31:29 +0100

    taking shape nicely. delete/submit/cancel now supported.

--- phileas/admin/test/MEW_extra/Admin/edit.py
+++ phileas/admin/test/MEW_extra/Admin/edit.py
@@ -28,15 +28,29 @@ class MEW_AdminEditPage(Page):
         self.submitting = os.environ.get("REQUEST_METHOD") == "POST"
         if self.submitting:
             self.form = cgi.FieldStorage()
-            answers = [(mfs.name, mfs.value) for mfs in self.form.list if not mfs.name.endswith('_')]
-            #print(answers, file=sys.stderr)
-            try:
-                self.lid_ = Lid(**dict(answers))
-                return self.update_and_show_index()
-
-            except EntityError as ee:
-                print(ee, file=sys.stderr)
-                self.ee = ee
+            requested_action = self.form.getfirst('button_')
+            if requested_action == 'Submit':
+                answers = [(mfs.name, mfs.value) for mfs in self.form.list if not mfs.name.endswith('_')]
+                #print(answers, file=sys.stderr)
+                try:
+                    self.lid_ = Lid(**dict(answers))
+                except EntityError as ee:
+                    print(ee, file=sys.stderr)
+                    self.ee = ee
+            if self.ee is None:
+                # incorporate the updated entry into the module:
+                if requested_action != 'Cancel':
+                    with open(MEW_members_file, 'w') as module_src:
+                        module_src.writelines(self.all_lines[:self.line_[0]])
+                        if requested_action != 'Delete':
+                            module_src.write(str(self.lid_))
+                        module_src.writelines(self.all_lines[self.line_[1]:])
+                # from index import MEW_AdminIndexPage
+                # pg = MEW_AdminIndexPage()
+                # pg.main()
+                # sys.exit(0)
+                subprocess.run('./index.py')
+                return None
         else:
             item_string = ''.join(self.all_lines[slice(*self.line_)])
             #print(item_string, file=sys.stderr)
@@ -44,19 +58,6 @@ class MEW_AdminEditPage(Page):
 
         return Page.validate(self, **kw)
 
-    def update_and_show_index(self):
-        # The changes appear to be ok!
-        # incorporate the updated entry into the module:
-        if 1:
-            with open(MEW_members_file, 'w') as module_src:
-                module_src.writelines(self.all_lines[:self.line_[0]])
-                module_src.write(str(self.lid_))
-                module_src.writelines(self.all_lines[self.line_[1]:])
-        #from index import MEW_AdminIndexPage
-        #pg = MEW_AdminIndexPage()
-        #pg.main()
-        #sys.exit(0)
-        subprocess.run('./index.py')
 
     def entry_line(self, displayed_name, attr_name, placeholder):
         colour = '#000000'  # black is default
@@ -85,11 +86,16 @@ class MEW_AdminEditPage(Page):
                  ('evt. 2e emailadress', 'altEmailAddress', 'optional'),
                  ('geboortedatum', 'birthDate', 'e.g. 15-mrt-1963'),
                  ('lidmaatschap datum', 'memberSince', 'e.g. 15-okt-2003'),
-             )],
-            '<input type = "submit" value = "Submit" />'
-            ),
+            )],
+            [(h.input(type = "submit", name="button_", STYLE="background-color:%s" % colour, value=val_) | '')
+             for val_, colour in (
+                ("Cancel", "green"),
+                ("Submit", "orange"),
+                ("Delete", "red"),
+            )],
+
             h.a(STYLE="color:#ff0000;") | self.ee
-        )
+        ))
         # return (h | 'stub for body of MEW_AdminEditPage')
 if __name__ == "__main__":
     # print ("hello Larry")
