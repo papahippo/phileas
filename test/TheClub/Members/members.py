d005b60bff09678e682b2ebdfc6ccf61a695b704
papahippo <hippostech@gmail.com>
2018-11-20 22:32:12 +0100

    some files had not been added to git!

---
+++ test/TheClub/Members/members.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+# -*- encoding: utf8 -*-
+from entity.club import Member
+from .mailgroups import *
+
+Daffie = Member(
+    name='Duck, Dafne',
+    initials='',
+    called='Daffie',
+    streetAddress='De Vijfer 30',
+    postCode='1234 AC',
+    cityAddress='Eendhoven',
+    phone='099-1234567',
+    mobile='06-9876543',
+    emailAddress='dafne.duck2@wetmail.com secnd very long email address',
+    altEmailAddress='',
+    birthDate='12-aug-1953',
+    memberSince='03-jul-2017',
+    instrument='Clarinet',
+    mailGroups=[]
+)
+
+Donald = Member(
+    name='Duck, Donaldus',
+    initials='',
+    called='Donald',
+    streetAddress='De Vijfer 30a',
+    postCode='1234 AB',
+    cityAddress='Eendhoven',
+    phone='099-1234567',
+    mobile='06-9876543',
+    emailAddress='donald.duck42@wetmail.com',
+    altEmailAddress='',
+    birthDate='12-aug-1951',
+    memberSince='03-okt-2017',
+    instrument='Flute',
+    mailGroups=[]
+)
+
+newbie_twoshoes = Member(
+    name='Doubleshod, Novus',
+    initials='',
+    called='newbie twoshoes',
+    streetAddress='The Hovel',
+    postCode='US99 4ME',
+    cityAddress='',
+    phone='',
+    mobile='',
+    emailAddress='',
+    altEmailAddress='',
+    birthDate='',
+    memberSince='',
+    instrument='',
+    mailGroups=[]
+)
+
---
+++ test/TheClub/indexPage.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# -*- encoding: utf8 -*-
+from .clubPage import ClubPage
+class IndexPage(ClubPage):
+    pass
