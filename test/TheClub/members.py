61b5edac354d32a585bdada79db8138df65f31a0
Larry Myerscough <hippostech@gmail.com>
2019-01-08 21:29:53 +0100

    just member added

--- test/TheClub/members.py
+++ test/TheClub/members.py
@@ -1,7 +1,38 @@
-#!/usr/bin/python3
-# -*- encoding: utf8 -*-
 from entity.club import Member
-from .mailgroups import *
+
+new__Kid__On__The__Block = Member(
+    name='Boy, Lost',
+    initials='',
+    called='new-Kid-On-The-Block',
+    streetAddress='',
+    postCode='',
+    cityAddress='',
+    phone=[],
+    mobile=[],
+    emailAddress=[],
+    altEmailAddress=[],
+    birthDate='',
+    memberSince='',
+    instrument='',
+    mailGroups=[]
+)
+
+newby = Member(
+    name='Twoshoes, newby',
+    initials='',
+    called='newby',
+    streetAddress='',
+    postCode='',
+    cityAddress='',
+    phone=[],
+    mobile=[],
+    emailAddress=[],
+    altEmailAddress=[],
+    birthDate='',
+    memberSince='',
+    instrument='',
+    mailGroups=[]
+)
 
 Daffie = Member(
     name='Duck, Dafne',
@@ -10,10 +41,10 @@ Daffie = Member(
     streetAddress='De Vijfer 30',
     postCode='1234 AC',
     cityAddress='Eendhoven',
-    phone='099-1234567',
-    mobile='06-9876543',
-    emailAddress='dafne.duck2@wetmail.com secnd very long email address',
-    altEmailAddress='',
+    phone=["099-1234567"],
+    mobile=["06-9876543"],
+    emailAddress=["dafne.duck2@wetmail.com", "second very long email address"],
+    altEmailAddress=[],
     birthDate='12-aug-1953',
     memberSince='03-jul-2017',
     instrument='Clarinet',
@@ -27,30 +58,13 @@ Donald = Member(
     streetAddress='De Vijfer 30B',
     postCode='1234 AB',
     cityAddress='Eendhoven',
-    phone='099-1234567',
-    mobile='06-9876543',
-    emailAddress='donald.duck42@wetmail.com',
-    altEmailAddress='',
+    phone=["099-1234567"],
+    mobile=["06-9876543"],
+    emailAddress=["donald.duck42@wetmail.com"],
+    altEmailAddress=[],
     birthDate='12-aug-1951',
     memberSince='03-okt-2017',
     instrument='Flute',
-    mailGroups=[]
-)
-
-new__Kid__On__The__Block = Member(
-    name='',
-    initials='',
-    called='new-Kid-On-The-Block',
-    streetAddress='',
-    postCode='',
-    cityAddress='',
-    phone='',
-    mobile='',
-    emailAddress='',
-    altEmailAddress='',
-    birthDate='',
-    memberSince='',
-    instrument='',
     mailGroups=[]
 )
 
